c5e9e6e34aab0d4111493663543856ec001a30d1
ctoic <i202612@nu.edu.pk>
2024-07-17 17:00:21 +0500

    added string operations

---
+++ variable.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variables</title>
+</head>
+<body>
+    <?php 
+        // integer data type 
+        $age = 21;
+        $name = "najam";
+        $ismale = "yes";
+        $gpa = 2.67;
+
+        echo $age;
+        echo $name;
+        echo $ismale;
+        echo $gpa;
+
+        echo strtoupper($name);
+
+        ?>
+    
+</body>
+</html>
