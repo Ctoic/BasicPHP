8fe0645a28bdde62a3d5334045ce7a9a50a8b2bc
ctoic <i202612@nu.edu.pk>
2024-07-17 16:28:56 +0500

    basic php concepts were added

---
+++ site.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sitestoic</title>
+</head>
+<body>
+    <?php 
+        echo"<h1>Stoic's Site</h1>";
+        echo"<hr>";
+
+        $charcter = "budha";
+        $charter_interest = "Space";
+
+
+        echo "<p>hello this is the first ever php site created by me </p>";
+        echo "<p>It's funny but in php your keywords are not case sensetive but your variables are </p>";
+        echo "<p> A Programmer Name Najam lived in the mountains.</p>";
+        echo "<p> $charcter was a $charter_interest  freak.</p>";
+        echo "<p> $charcter's Imagination was better then his coding skills.</p>";
+        echo "<p> $charcter learned the skill of being the creator.</p>"
+
+
+        ?>
+
+
+
+    
+</body>
+</html>
