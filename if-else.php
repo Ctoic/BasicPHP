e065961292971ab6c032e707c9f2b8e2ca8dccea
ctoic <i202612@nu.edu.pk>
2024-07-19 14:44:33 +0500

    added more if-else

--- if-else.php
+++ if-else.php
@@ -1,4 +1,33 @@
 <?php 
-    // creating program for if and else 
     
+    //take marks input from user 
+    $marks  = $$_GET["marks"];
+    if($marks >= 80 && $marks <= 100)
+    {
+        echo "A+";
+    }
+    else if($marks >= 70 && $marks <= 79)
+    {
+        echo "A";
+    }
+    else if($marks >= 60 && $marks <= 69)
+    {
+        echo "B";
+    }
+    else if($marks >= 50 && $marks <= 59)
+    {
+        echo "C";
+    }
+    else if($marks >= 40 && $marks <= 49)
+    {
+        echo "D";
+    }
+    else if($marks >= 33 && $marks <= 39)
+    {
+        echo "E";
+    }
+    else if($marks >= 0 && $marks <= 32)
+    {
+        echo "F";
+    }
     ?>
